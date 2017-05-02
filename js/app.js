6af47a96e87ad29de59a1a5b3322d1ce1cbcaa8d
EmmAlva <clioemma_28@hotmail.com>
2017-05-01 20:14:58 -0500

    try1

---
+++ js/app.js
@@ -0,0 +1,13 @@
+var registro = document.getElementById("registroPublico");
+
+var publico-si = document.getElementById("publico-si").value;
+var publico-no = document.getElementById("publico-no").value;
+
+function mostrar(){
+	publico-si.addEventListener('click',function(){
+		registro.style.display = "block";
+	})
+	publico-no.addEventListener('click',function(){
+		registro.style.display = "none";
+	}
+}
